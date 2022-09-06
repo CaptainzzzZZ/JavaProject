1bae6950e4da4894eeca8914eeb534b3a41b9f94
小醒目 <1779289666@qq.com>
2022-09-06 23:13:29 +0800

    第一个Java程序

---
+++ test_9_6.java
@@ -0,0 +1,31 @@
+package test_9_6;
+
+public class test_9_6 {
+	public static void main(String[] args)
+	{
+		/*
+		 * int num=4; byte b=num; System.out.println(b);
+		 */
+		
+		/*
+		 * byte a; int b=666; a=(byte)b; System.out.println("b="+b);
+		 * System.out.println("a="+a);
+		 */
+		
+		/*
+		 * byte a1=3; byte a2=4; byte a3=a1+a2; System.out.println("a3="+a3);
+		 */
+		
+		int x=12;
+		{
+			int y=96;
+			System.out.println("x is "+x);
+			System.out.println("y is "+y);
+			y=x;
+		}
+		
+		System.out.println("x is "+x);
+		
+	}
+
+}
