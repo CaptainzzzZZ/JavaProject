d558774241065f3d0328d1e062f45c17e93dd8c7
小醒目 <1779289666@qq.com>
2022-09-09 22:57:41 +0800

    初识Java

---
+++ Test_9_9.java
@@ -0,0 +1,63 @@
+package test_9_8;
+
+public class Test_9_9 {
+    public static void main(String[] args) {
+//        short a=5;
+//        int b=5;
+//        a+=b;//隐式强制类型转换
+//        System.out.println("abc:"+a);//+是一个连接字符串的符号
+//        System.out.println("hello "+"new"+"code");
+
+//        int grade=75;
+//        if(grade>80){
+//            System.out.println("优");
+//        }
+//        else if (grade>70) {
+//            System.out.println("良");
+//        }
+//        else if(grade>60) {
+//            System.out.println("中");
+//        }
+//        else if(grade<60){
+//            System.out.println("差");
+//        }
+
+//        int a=0;
+//        int b=0;
+//        int c=0;
+//        boolean x,y;
+//        x=a>0&b++>0;
+//        System.out.println(x);
+//        y=b>0&&c++>1;//前面是对的继续判断后边；
+//        System.out.println(y);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_9_8.java
@@ -0,0 +1,17 @@
+package test_9_8;
+
+public class test_9_8 {
+//    public static  void main(String[] args){
+////        System.out.println("hello world");
+//
+////        int []arr={0,1,2,3,4,5,6,7,8,9};
+////        int i;
+////        for(i=0;i< arr.length;i++){
+////            System.out.println(arr[i]);
+////        }
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
