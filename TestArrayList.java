1a004de2fbb8057fc56af2d9ef82d0b78424e929
小醒目 <1779289666@qq.com>
2022-10-29 17:50:41 +0800

    顺序表基本实现增删查改判空判满输出

--- TestArrayList.java
+++ TestArrayList.java
@@ -4,6 +4,11 @@ public class TestArrayList {
     private int []elem;
     private int size;
     private static final int capacity=10;
+
+    public TestArrayList() {
+        this.elem = new int[capacity];
+        this.size = 0;
+    }
     private boolean isFull(){
 //        if(this.size==this.elem.length){
 //            return true;
@@ -11,10 +16,8 @@ public class TestArrayList {
 //        return false;
         return this.size==this.elem.length;
     }
-
-    public TestArrayList() {
-        this.elem = new int[capacity];
-        this.size = 0;
+    public boolean isEmpty(){
+        return this.size==0;
     }
     public void insert(int pos,int num){
         //判断顺序表是否满
@@ -34,9 +37,10 @@ public class TestArrayList {
         this.size++;
     }
     public void printList(){
-        for (int i = 0; i < this.size; i++) {
-            System.out.print(this.elem[i]+" ");
-        }
+//        for (int i = 0; i < this.size; i++) {
+//            System.out.print(this.elem[i]+" ");
+//        }
+        System.out.println(Arrays.toString(this.elem));
     }
     public int findList(int num){
         for (int i = 0; i < this.size; i++) {
@@ -46,6 +50,17 @@ public class TestArrayList {
         }
         return -1;
     }
+    public int getPos(int pos){
+        if(elem==null){
+            throw new RuntimeException("pos位置不合法");
+        }
+        if(pos<0||pos>=this.size){
+            throw new RuntimeException("pos位置不合法");
+
+        }
+        return this.elem[pos];
+    }
+
     public void popList(int pos) {
         for (int i = pos; i < this.size - 1; i++) {
             this.elem[i] = this.elem[i + 1];
@@ -53,18 +68,23 @@ public class TestArrayList {
         this.size--;
     }
     public void modifyList(int pos,int num){
+        if(elem==null){
+            throw new RuntimeException("pos位置不合法");
+        }
+        if(pos<0||pos>=this.size){
+            throw new RuntimeException("pos位置不合法");
+        }
         this.elem[pos]=num;
     }
     public static void main(String[] args) {
         TestArrayList arrlist=new TestArrayList();
+        arrlist.insert(0,44);
         arrlist.insert(0,11);
         arrlist.insert(0,22);
         arrlist.insert(0,33);
-        arrlist.insert(0,44);
+
         int pos=arrlist.findList(44);
         arrlist.popList(pos);
-        arrlist.modifyList(pos,55);
-
         arrlist.printList();
     }
 }
