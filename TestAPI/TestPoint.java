6c7836a00a6f1c6b99e78f2efb8453abfeb24fa9
小醒目 <1779289666@qq.com>
2022-11-03 00:03:02 +0800

    API的String.toString() equals() charAT() indexOf() trim() substring()返回子串 matches(regex) StringBuilder.append()  System.currentTimeMillis()

--- TestAPI/TestPoint.java
+++ TestAPI/TestPoint.java
@@ -18,13 +18,67 @@ public class TestPoint {
 		}
 		return s.substring(0, index);
 	}
-
+	public static boolean ishuiwen(String str) {
+		String cur=str.trim();
+		for(int i=0, j=cur.length()-1;i<j;i++,j--) {
+			if(cur.charAt(i)!=cur.charAt(j)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	public static void main(String[] args) {
+		System.out.println("请输入一个身份证号：");
+		Scanner sc	= new Scanner(System.in);
+		String str=sc.nextLine();
+		String regex="\\d{17}[\\dxX]";
+		if(str.matches(regex)) {
+			System.out.println("格式正确");
+		}
+		else {
+			System.out.println("格式错误");
+		}
+	}
+	public static void main6(String[] args) {
+		String str0="abcdefghijklmnopqrstuvwxyz";
+		StringBuilder sb=new StringBuilder();
+		for(int i=0;i<2;i++)
+			sb.append(str0);
+		System.out.println(sb);
+		
+	}
+	public static void main5(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		boolean ret=ishuiwen(str);
+		if(ret) {
+			System.out.println("回文串");
+		}
+		else {
+			System.out.println("不是回文");
+		}
+		
+	}
+	public static void main4(String[] args) {
+		String str2="    abcoocdeoofghoo   ";
+		System.out.println(str2.indexOf("oo"));
+		System.out.println(str2.indexOf("ooo"));//-1
+		System.out.println(str2.trim());
+		System.out.println(str2.replace("oo", "mm"));
+		String[] arrstr=str2.split("oo");
+		for(String s:arrstr) {
+			System.out.println(s);
+		}
+		System.out.println(str2.toUpperCase());
+		System.out.println(str2.toCharArray());
+		String str3="123456789";
+		System.out.println(str3.valueOf(str3));
+	}
+	public static void main3(String[] args) {
 		System.out.println("请输入一个Email：");
 		String str = new Scanner(System.in).next();
 		String s = getName(str);
 		System.out.println(s);
-
 	}
 
 	public static void main2(String[] args) {
