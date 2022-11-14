cd5096cdb895d5b14c38a36a5d6147cbc8cdf34f
小醒目 <1779289666@qq.com>
2022-11-13 22:59:12 -0600

    Integer.valueOf包装基本类型 BigDecimal精确类型 BigInteger超长整型  DateSimpleDateFormat

--- Wrap.java
+++ Wrap.java
@@ -1,8 +1,44 @@
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Wrap {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc=new Scanner(System.in);
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("请输入yyyy-MM-dd");
+        String str=sc.nextLine();
+        Date d=sdf.parse(str);
+        long t=System.currentTimeMillis()-d.getTime();
+        t=t/1000/60/60/24;
+        System.out.println(t);
+
+
+    }
+    public static void main3(String[] args) {
+        Date d1=new Date();
+        Date d2=new Date(9000000000000L);
+        System.out.println(d1.getTime());
+        System.out.println(d2.getTime());
+        d1.setTime(0L);
+        System.out.println(d1.compareTo(d2));
+        SimpleDateFormat d3=new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String str1=d3.format(d1);
+        String Str2=d3.format(d2);
+        System.out.println(str1);
+        System.out.println(Str2);
+    }
+    public static void main2(String[] args) {
+        BigInteger bi=BigInteger.valueOf(66);
+        for (int i = 1; i < bi.longValue() ; i++) {
+            bi=bi.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(bi);
+    }
+    public static void main1(String[] args) {
         Integer a=new Integer(5);
         Integer b=Integer.valueOf(5);
         Integer c=Integer.valueOf(5);
@@ -40,7 +76,11 @@ public class Wrap {
         System.out.println(bd3.doubleValue());
         bd3=bd1.multiply(bd2);
         System.out.println(bd3.doubleValue());
-        bd3=bd1.divide(bd2,10,BigDecimal.ROUND_HALF_DOWN);
+        bd3=bd1.divide(bd2,50,BigDecimal.ROUND_HALF_UP);
+        System.out.println(bd3.doubleValue());
+        System.out.println(bd3.toString());
+
+        bd3= bd3.setScale(2,BigDecimal.ROUND_HALF_UP);//newScale是小数点后几位  对一个数舍入运算
         System.out.println(bd3.doubleValue());
 
 
