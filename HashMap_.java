f0ef0ec4cc5e6980491496770ccbee033eb2befe
小醒目 <1779289666@qq.com>
2022-12-14 01:07:41 +0800

    HashMap_练习

--- HashMap_.java
+++ HashMap_.java
@@ -1,38 +1,30 @@
 package Map_;
 
+import javafx.concurrent.Worker;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-class worker{
-    private String id;
-    private String salary;
+class Emp{
     private String name;
+    private double sal;
+    private int id;
 
-    public worker(String salary, String name) {
-        this.salary = salary;
+    public Emp(String name, double sal, int id) {
         this.name = name;
-    }
-
-    public worker(String id) {
+        this.sal = sal;
         this.id = id;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSalary() {
-        return salary;
-    }
-
-    public void setSalary(String salary) {
-        this.salary = salary;
+    @Override
+    public String toString() {
+        return "worker{" +
+                "name='" + name + '\'' +
+                ", sal=" + sal +
+                ", id=" + id +
+                '}';
     }
 
     public String getName() {
@@ -43,30 +35,47 @@ class worker{
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "id"+this.id+"sal"+this.salary+"name"+this.name;
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
 public class HashMap_ {
     public static void main(String[] args) {
-        HashMap<Object, Object> hashMap = new HashMap<>();
-        hashMap.put(new worker("001"),new worker("5555","lzz"));
-        hashMap.put(new worker("002"),new worker("3333","xxm"));
-        hashMap.put(new worker("003"),new worker("6666","LZZZ"));
-        Set<Map.Entry<Object, Object>> entrySet = hashMap.entrySet();
-        for (Object entryset :entrySet) {
-            Map.Entry entry=(Map.Entry)entryset;
-            System.out.println(entry.getKey()+"--"+entry.getValue());
+        HashMap hashMap = new HashMap();
+        hashMap.put(1,new Emp("lzz",50000,1));
+        hashMap.put(2,new Emp("xxm",6000,2));
+        hashMap.put(3,new Emp("zzz",1000,3));
+        Set entrySet = hashMap.entrySet();
+        Iterator iterator = entrySet.iterator();
+        while (iterator.hasNext()) {
+            Object next =  iterator.next();
+            Map.Entry entry=(Map.Entry)next;
+            Emp emp=(Emp) entry.getValue();
+            if(emp.getSal()>2000){
+                System.out.println(emp);
+            }
         }
-
-        Set<Object> keySet = hashMap.keySet();
-        for (Object o :keySet) {
-            System.out.println(o+"-"+hashMap.values());
+        Set keySet = hashMap.keySet();
+        for (Object key :keySet) {
+            Emp emp2 =(Emp) hashMap.get(key);
+            if (emp2.getSal()>2000){
+                System.out.println(emp2);
+            }
         }
-        Iterator it= keySet.iterator();
-
-
 
     }
+
+
 }
