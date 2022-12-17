f00c6e66aee9ebd7e6a61cf71abf4d09e4eee1bb
小醒目 <1779289666@qq.com>
2022-12-17 23:15:50 +0700

    蓝桥

--- DayDayLeetcode.java
+++ DayDayLeetcode.java
@@ -5,7 +5,164 @@ import java.util.*;
 
 @SuppressWarnings({"all"})
 public class DayDayLeetcode {
+    public static boolean is24(int n){
+        while(n!=0){
+            if(n%10==2||n%10==4){
+                return false;
+            }
+            n/=10;
+        }
+        return true;
+    }
     public static void main(String[] args) {
+        int ret=0;
+        for (int i = 0; i <=2019; i++) {
+            for (int j = 0; j <=2019 ; j++) {
+                int k=2019-i-j;
+                if (is24(i)&&is24(j)&&is24(k)){
+                    if(i!=j&&j!=k&&i!=k&&i<j&&j<k){
+                        ret++;
+                    }
+                }
+            }
+        }
+        System.out.println(ret);
+    }
+    public static void main41(String[] args) {
+        long[] arr = new long[20190324];
+        arr[0]=1;
+        arr[1]=1;
+        arr[2]=1;
+        for (int i = 3; i < 20190324; i++) {
+            arr[i]=(arr[i-1]+arr[i-2]+arr[i-3])%10000;
+            if (i==20190323)
+                System.out.println(arr[i]);
+        }
+    }
+    public static boolean isprimary(int n){
+        for (int i = 2; i <n ; i++) {
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main40(String[] args) {
+        for (int i = 2;i<10000 ; i++) {
+            for (int j = 1; j <1000 ; j++) {
+                int k=0;
+                for ( k = 0; k <=9 ; k++) {
+                    if(!isprimary(i+k*j)){
+                        break;
+                    }
+                }
+                if (k==10){
+                    System.out.println(j);
+                    return ;
+                }
+            }
+        }
+    }
+    public static void main39(String[] args) {
+        int yyyy,mm,dd;
+        int sum=0;
+        int week=5;
+        int[] months={0,31,28,31,30,31,30,31,31,30,31,30,31};
+        for ( yyyy = 2000; yyyy <=2020 ; yyyy++) {
+            if(yyyy%4==0&&yyyy%100!=0||yyyy%400==0){
+                months[2]=29;
+            }
+            else{
+                months[2]=28;
+            }
+            for (mm=1;mm<=12;mm++){
+                for (dd=1;dd<months[mm];dd++){
+                    sum++;
+                    week%=7;
+                    if (week==1||dd==1){
+                        sum++;
+                    }
+                    week++;
+                    if (yyyy==2020&&mm==10&&dd==1){
+                        System.out.println(sum);
+                        return;
+                    }
+                }
+            }
+
+        }
+
+    }
+    public static void main38(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int yyyy= scanner.nextInt();
+        if (yyyy>9999){
+            return;
+        }
+        String[] ten={"jia","yi","bing","ding","wu","ji","geng","xin","ren","gui"};
+        String[] twen={"zi","chou","yan","mao","chen","si","wu","wei","shen","you","xu","hai"};
+        int index=6;//公元元年是公元1年是辛酉年
+        int index2=8;//公元0年就是庚申年
+        for (int i = 0; i < yyyy; i++) {
+            index= (index+1)%10;
+            index2=(index2+1)%12;
+        }
+        System.out.println(ten[index]+twen[index2]);
+
+    }
+    public static void main37(String[] args) {
+        for (double q = 0; q <0.5 ; q=q+0.0000001) {
+            if((Math.pow((1+q),4))*(Math.pow((1+0.6*q),4))==Math.pow(1.95,4)){
+                System.out.println(q);
+            }
+        }
+        System.out.println(-1);
+    }
+    public static void main36(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        int range=arr[arr.length-1]-arr[0];
+        int min=range;
+        for (int i = 0; i <arr.length-1; i++) {
+            int t=arr[i+1]-arr[i];
+            if(t<min){
+                min=t;
+            }
+        }
+        System.out.println(range/min+1);
+
+    }
+    public static void main35(String[] args) {
+        for (int i = 1; i <=1000000007 ; i++) {
+            if (i*2021%1000000007==999999999){
+                System.out.println(i);
+                break;
+            }
+            else System.out.println(0);
+            break;
+        }
+    }
+    public static void main34(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str= scanner.nextLine();
+        StringBuilder stringBuilder=new StringBuilder(str);
+        int t= scanner.nextInt();
+        for (int i = 0; i <t ; i++) {
+            for (int j = 0; j < stringBuilder.length()-1; j++) {
+                if(stringBuilder.charAt(j)>stringBuilder.charAt(j+1)){
+                    stringBuilder.delete(j,j+1);
+                    break;
+                }
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+    public static void main33(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
         char[] chars = {'A','B','C','D','E','F','G','H','I','J','K',
                           'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
